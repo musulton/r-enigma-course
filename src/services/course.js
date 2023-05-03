43fd521cf2419ea319df683722d98acb226b17db
Muhammad Sulton <musulton@gmail.com>
2023-05-03 11:38:27 +0700

    Remove adding element to document html

--- src/services/course.js
+++ src/services/course.js
@@ -36,7 +36,6 @@ export const downloadCourseFile = async (filename) => {
         const link = document.createElement('a')
         link.href = url
         link.setAttribute('download', filename)
-        document.body.appendChild(link)
         link.click()
     } catch (e) {
         console.log(e)
